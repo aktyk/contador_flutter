cfe71bc2e6bd92adfce517a4abcb880f914c0560
Juan Miguel <aktyk.117@gmail.com>
2021-10-06 22:47:08 -0500

    Initial commit

---
+++ lib/src/pages/contador_page.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+class ContadorPage extends StatefulWidget {
+  @override
+  createState() {
+    return _ContadorPageState();
+  }
+}
+
+// con el _ en el nombre de la clase se indica que es privada
+class _ContadorPageState extends State<ContadorPage> {
+  final _estiloTexto = new TextStyle(fontSize: 25);
+  int _conteo = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Contador numérico'),
+          centerTitle: true,
+        ),
+        body: Center(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Número de clicks:',
+              style: _estiloTexto,
+            ),
+            Text(
+              // Interpolación de string
+              '$_conteo',
+              style: _estiloTexto,
+            ),
+          ],
+        )),
+        floatingActionButton: _crearBotones()
+        //floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
+        );
+  }
+
+  Widget _crearBotones() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: <Widget>[
+        SizedBox(width: 30.0),
+        FloatingActionButton(
+            child: Icon(Icons.exposure_zero), onPressed: _reset),
+        Expanded(child: SizedBox()),
+        FloatingActionButton(child: Icon(Icons.remove), onPressed: _sustraer),
+        SizedBox(width: 5.0),
+        // La función _agregar no lleva los paréntesis para evitar que se construya
+        // cuando se inicializa la clase.
+        // Se puede usar el valor de null en onPressed para deshabilitar la función del botón.
+        FloatingActionButton(child: Icon(Icons.add), onPressed: _agregar),
+      ],
+    );
+  }
+
+  // Método privado
+  void _agregar() {
+    setState(() {
+      _conteo++;
+    });
+  }
+
+  void _sustraer() {
+    setState(() => _conteo--);
+  }
+
+  void _reset() {
+    setState(() => _conteo = 0);
+  }
+}
